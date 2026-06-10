c1290b32e53fc71254a8fa77f6ccccb01efc6a34
Himanshu Mittal <himanshu1210mittal@gmail.com>
2026-06-10 16:10:35 +0530

    first commit

---
+++ app/profile/account/page.tsx
@@ -0,0 +1,5 @@
+export default function account() {
+    return(
+        <div className="flex flex-col flex-1 justify-center items-center">account</div>
+    )
+}
---
+++ app/profile/page.tsx
@@ -0,0 +1,6 @@
+
+export default function page ()  {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center">page</div>
+  )
+}
