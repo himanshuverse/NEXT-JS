8e2625e3e7446e8ab62e72f3dc60f925490cc46c
Himanshu Mittal <himanshu1210mittal@gmail.com>
2026-06-10 17:19:44 +0530

    dynamic routes with ex of products detail

---
+++ app/profile/products/[slug]/page.tsx
@@ -0,0 +1,53 @@
+import { notFound } from "next/navigation";
+
+const products = [
+  {
+    id: 1,
+    name: "Laptop",
+    price: 50000,
+  },
+  {
+    id: 2,
+    name: "Phone",
+    price: 25000,
+  },
+  {
+    id: 3,
+    name: "Headphones",
+    price: 3000,
+  },
+];
+
+export default async function ProductDetails({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+
+  const product = products.find(
+    (p) => p.id === Number(slug)
+  );
+
+  if (!product) {
+    notFound();
+  }
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black-100">
+      <div className="bg-white p-8 rounded-xl shadow-lg">
+        <h1 className="text-3xl font-bold text-black">
+          {product.name}
+        </h1>
+
+        <p className="text-xl text-green-600 mt-4">
+          ₹{product.price}
+        </p>
+
+        <p className="mt-4 text-gray-600">
+          Product ID: {product.id}
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ app/profile/products/page.tsx
@@ -0,0 +1,51 @@
+import Link from "next/link";
+
+export default function products(){
+     const products = [
+    {
+      id: 1,
+      name: "Laptop",
+      price: 50000,
+    },
+    {
+      id: 2,
+      name: "Phone",
+      price: 25000,
+    },
+    {
+      id: 3,
+      name: "Headphones",
+      price: 3000,
+    },
+  ];
+
+    return(
+       
+       <div className="min-h-screen bg-gray-100 p-8">
+  <h1 className="text-4xl font-bold text-center mb-10">
+    Products
+  </h1>
+
+  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+    {products.map((product) => (
+      <div
+        key={product.id}
+        className="bg-white rounded-xl shadow-md p-6 hover:shadow-xl transition duration-300"
+      >
+        <h2 className="text-xl font-semibold text-gray-800">
+          {product.name}
+        </h2>
+
+        <p className="text-2xl font-bold text-green-600 mt-3">
+          ${product.price}
+        </p>
+
+        
+          <Link className="mt-5 w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition" href={`/profile/products/${product.id}`}>see details</Link>
+     
+      </div>
+    ))}
+  </div>
+</div>
+    )
+}
