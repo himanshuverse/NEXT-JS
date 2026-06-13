666613da331f8e6028d240d6cc2fc0e4f324b4df
Himanshu Mittal <himanshu1210mittal@gmail.com>
2026-06-13 22:17:34 +0530

    userouter usepathname usesearchparams

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,6 +1,20 @@
+"use client"
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation"
 
 export default function page ()  {
+  const router =useRouter()
+  const pathname=usePathname()
+  const searchParams=useSearchParams()
+  console.log(searchParams.getAll("name"),searchParams.has("name"),"searchParams")
+  const handleNavigate=()=>{
+
+    router.push("/")
+  }
   return (
-    <div className="flex flex-col flex-1 items-center justify-center">page</div>
+    <div className="flex flex-col flex-1 items-center justify-center">
+    <h1>profile component</h1>
+    <button onClick={handleNavigate}>navigate to home page</button>
+    </div>
   )
 }
