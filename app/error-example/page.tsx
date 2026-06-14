0f399218d7a26df8ac02645726d5aae551fd331f
Himanshu Mittal <himanshu1210mittal@gmail.com>
2026-06-14 17:48:17 +0530

    error handling page

---
+++ app/error-example/page.tsx
@@ -0,0 +1,40 @@
+async function getProducts() {
+  //simulate a dummy error here
+  const shouldError = Math.random() > 0.5;
+
+  if (shouldError) {
+    throw new Error("Failed to fetch products");
+  }
+
+  return [
+    {
+      id: 1,
+      name: "One",
+    },
+    {
+      id: 2,
+      name: "Two",
+    },
+    {
+      id: 3,
+      name: "Three",
+    },
+  ];
+}
+
+export default async function ErrorExample() {
+  const products = await getProducts();
+
+  return (
+    <div className="p-4">
+      <h1>Products List</h1>
+      <div className="grid gap-4">
+        {products.map((product) => (
+          <div key={product.id}>
+            <p>{product.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
